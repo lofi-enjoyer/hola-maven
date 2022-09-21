05a7571e5d2b937bbcf34653f91916c97d38b1eb
lofienjoyer <25750692+lofi-enjoyer@users.noreply.github.com>
2022-09-21 09:20:13 +0100

    First commit

---
+++ src/main/java/dad/holamaven/App.java
@@ -0,0 +1,13 @@
+package dad.holamaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
